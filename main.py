1ccb1b95d3e1528a796fc6a8e8422c5489cc7b2b
Emmanuel Addo <addoemmanuel141509@gmail.com>
2026-07-04 11:17:13 +0000

    Add frontend CRA app, .gitignore, and update greet endpoint

--- main.py
+++ main.py
@@ -1,67 +1,8 @@
-from fastapi import FastAPI, Path
-from typing import Optional
-from pydantic import BaseModel
-
+from fastapi import FastAPI
 
 app = FastAPI()
 
-students = {
-    1: {
-        "name": "john",
-        "age": 17,
-        "year": "year 12"
-    }
-}
-
-
-class Student(BaseModel):
-    name: str
-    age: int
-    year: str
-
-
-class UpdateStudent(BaseModel):
-    name: Optional[str] = None
-    age: Optional[int] = None
-    year: Optional[str] = None
-
-
 @app.get("/")
-def index():
-    return {"name": "First name"}
+def greet():
+    return {"message": "Hello World"}
 
-
-@app.get("/get-student/{student_id}")
-def get_student(student_id: int):
-    return students[student_id]
-
-
-@app.get("/get-by-name")
-def get_students(*, name: Optional[str] = None, test: int):
-    for student_id in students:
-        if students[student_id]["name"] == name:
-            return students[student_id]
-        return {"Data": "Not found"}
-
-
-# post method
-@app.post("/create-student/{student_id}")
-def create_student(student_id: int, student: Student):
-    if student_id in students:
-        return {"Error": "Student exists"}
-    students[student_id] = student
-    return students[student_id]
-
-
-# put method
-@app.put("/update-student/{student_id}")
-def update_student(student_id: int, student: UpdateStudent):
-    if student_id not in students:
-        return {"Error": "Student does not exist"}
-    if student.name is not None:
-        students[student_id]["name"] = student.name
-    if student.age is not None:
-        students[student_id]["age"] = student.age
-    if student.year is not None:
-        students[student_id]["year"] = student.year
-    return students[student_id]
